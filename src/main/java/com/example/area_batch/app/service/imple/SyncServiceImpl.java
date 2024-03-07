c08d19ddb82b817dcfeec8e681f9d7336810930d
Israa-Aboelmagd <israa.aboelmagd@basatatech.com>
2024-03-07 15:45:04 +0200

    change the job parameter to update the job instance to accept ubadtle in DB

--- src/main/java/com/example/area_batch/app/service/imple/SyncServiceImpl.java
+++ src/main/java/com/example/area_batch/app/service/imple/SyncServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.area_batch.app.service.imple;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -21,9 +22,14 @@ public class SyncServiceImpl implements SyncService {
     private JobLauncher jobLauncher;
     private Job syncJob;
 
+     
     @Override
     public void sync() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
-        jobLauncher.run(syncJob,new JobParameters());
+        long currentTime = System.currentTimeMillis();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", currentTime)
+                .toJobParameters();
+        jobLauncher.run(syncJob,jobParameters);
     }
     
 }
